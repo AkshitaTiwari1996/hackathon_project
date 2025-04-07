61767312ee220deb4bcf1c2c6c6986c252046c4d
akshitatiwari <akshitatiwari2006@gmail.com>
2025-04-07 15:37:14 +0530

    Make the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -114,6 +114,15 @@ document.querySelectorAll('.js-add-to-cart')
                 
             });
         }//when button is clicked,product name will be out then add it to the cart
-        console.log(cart);
+        
+        let cartQuantity = 0;
+
+        cart.forEach((item) => {
+            cartQuantity += item.quantity;
+        });
+        
+        document.querySelector('.js-cart-quantity')
+        .innerHTML = cartQuantity;
+
     });
  });
